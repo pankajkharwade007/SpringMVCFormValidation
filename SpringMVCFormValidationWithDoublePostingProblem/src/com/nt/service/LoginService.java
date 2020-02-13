a0864d75470f80785c4ee0e62a8b0f5667adb69b
pankajkharwade007 <pankaj.kharwade.pk@gmail.com>
2020-02-13 16:19:11 +0530

    LoginSErvice is created

--- SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/service/LoginService.java
+++ SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/service/LoginService.java
@@ -1,5 +1,31 @@
 package com.nt.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nt.bo.UserBo;
+import com.nt.dao.LoginDao;
+import com.nt.dto.UserDto;
+
+@Service
 public class LoginService {
 
+	@Autowired
+	private LoginDao dao;
+	
+	public String authenticate(UserDto dto) {
+		UserBo bo=new UserBo();
+		bo.setUser(dto.getUser());
+		bo.setPwd(dto.getPwd());
+		
+		int cnt=dao.validate(bo);
+		
+		if(cnt!=0) {
+			return "InValid Credentials";
+		}else {
+			return "Valid Crederntials";
+		}
+		
+	}
+	
 }
