d7f1640524f59e18ab2523a9993c6bc6411130a2
pankajkharwade007 <pankaj.kharwade.pk@gmail.com>
2020-02-13 16:28:09 +0530

    Validator class is configured

--- SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/validator/LoginValidator.java
+++ SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/validator/LoginValidator.java
@@ -1,5 +1,34 @@
 package com.nt.validator;
 
-public class LoginValidator {
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
+import com.nt.commands.User;
+
+public class LoginValidator implements Validator{
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return clazz.isAssignableFrom(User.class);
+	}
+
+	@Override
+	public void validate(Object cmd, Errors errors) {
+		// TODO Auto-generated method stub
+		User ucmd=(User)cmd;
+		
+		if(ucmd.getUser()==null || ucmd.getUser().equals("") || ucmd.getUser().length()==0) {
+			errors.rejectValue("user", "user.required");
+		}
+		
+		if(ucmd.getPwd()==null || ucmd.getPwd().equals("") || ucmd.getPwd().length()==0) {
+			errors.rejectValue("pwd", "pwd.required");
+		}
+		
+		
+	}
+
+	
+	
 }
