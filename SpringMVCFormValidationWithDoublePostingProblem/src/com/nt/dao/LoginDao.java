f414a1cb16be2fa171b7b393339b1777c03a784b
pankajkharwade007 <pankaj.kharwade.pk@gmail.com>
2020-02-13 16:20:19 +0530

    LoginDao is configured

--- SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/dao/LoginDao.java
+++ SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/dao/LoginDao.java
@@ -1,7 +1,24 @@
 package com.nt.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.nt.bo.UserBo;
+
+@Repository
 public class LoginDao {
 
+	private static final String AUTh_QRY="SELECT COUNT(*) FROM USERLIST WHERE UNAME=? AND PWD=?";
+	
+	@Autowired
+	private JdbcTemplate jt;
+	
+	public int validate(UserBo bo) {
+		
+		int cnt=jt.queryForObject(AUTh_QRY,new Object[] {bo.getUser(),bo.getPwd()}, Integer.class);
+		return cnt;
+	}
 	
 	
 }
