d558a347883742ec1d852e2b9a8e21e3793bcb97
pankajkharwade007 <pankaj.kharwade.pk@gmail.com>
2020-02-13 14:52:30 +0530

    User class is created

--- SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/commands/User.java
+++ SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/commands/User.java
@@ -1,5 +1,36 @@
 package com.nt.commands;
 
+import java.util.Date;
+
 public class User {
 
+	private String user,pwd,dmn;
+	private Date dor;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getDmn() {
+		return dmn;
+	}
+	public void setDmn(String dmn) {
+		this.dmn = dmn;
+	}
+	public Date getDor() {
+		return dor;
+	}
+	public void setDor(Date dor) {
+		this.dor = dor;
+	}
+	
+	
+	
 }
