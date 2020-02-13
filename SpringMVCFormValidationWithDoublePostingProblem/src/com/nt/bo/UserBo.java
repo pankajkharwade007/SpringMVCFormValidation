29b91fd09724e2e234611b5fd5858192bccd504f
pankajkharwade007 <pankaj.kharwade.pk@gmail.com>
2020-02-13 14:56:42 +0530

    UserBo is created

--- SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/bo/UserBo.java
+++ SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/bo/UserBo.java
@@ -2,4 +2,24 @@ package com.nt.bo;
 
 public class UserBo {
 
+	private String user,pwd;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	
 }
--- SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/dao/LoginDao.java
+++ SpringMVCFormValidationWithDoublePostingProblem/src/com/nt/dao/LoginDao.java
@@ -2,4 +2,6 @@ package com.nt.dao;
 
 public class LoginDao {
 
+	
+	
 }
